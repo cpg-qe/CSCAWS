b4b887f2b54169e3a55490ecc0077d2b323091b4
cpg-qe <swathi.g@servicenow.com>
2026-03-04 17:35:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -19,10 +19,12 @@ variable "vpc_id" {
 
 variable "vpc_cidr" {
   type = string
+default = 10.0.0.0/16
 }
 
 variable "subnet_number" {
   type = number
+default = 120
 }
 variable "subnet_cidr" {
   type    = string
