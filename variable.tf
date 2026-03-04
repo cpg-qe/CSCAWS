ffcbcbda69cd30a72325be09583abaf2ec7da680
cpg-qe <swathi.g@servicenow.com>
2026-03-04 15:09:38 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,27 +1,34 @@
 variable "region" {
-  type = string
+  type    = string
+  default = "us-east-1"
 }
 
 variable "vpc_id" {
-  type = string
+  type    = string
+  default = "vpc-046d9499fabb0b219"
 }
 
 variable "subnet_cidr" {
-  type = string
+  type    = string
+  default = "10.0.50.0/24"
 }
 
 variable "ami" {
-  type = string
+  type    = string
+  default = "ami-0c02fb55956c7d316"
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
+  default = "t2.micro"
 }
 
 variable "instance_name" {
-  type = string
+  type    = string
+  default = "terraform-ec2"
 }
 
 variable "environment" {
-  type = string
+  type    = string
+  default = "dev"
 }
